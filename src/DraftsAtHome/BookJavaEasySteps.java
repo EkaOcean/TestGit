ac3e38b9cc31fca95803c3299ae8b1633576738e
EkaOcean <Katrin500@rambler.ru>
2022-09-03 21:18:42 +0700

    Initial commit

---
+++ src/DraftsAtHome/BookJavaEasySteps.java
@@ -0,0 +1,10 @@
+package DraftsAtHome;
+
+import java.sql.SQLOutput;
+
+public class BookJavaEasySteps {
+
+    public static void main(String[] args) {
+        System.out.println("Проверка Git");
+    }
+}
